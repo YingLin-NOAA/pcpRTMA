2af3622069796d8c8b2bb82069f6621ea3b06f62
YingLin-NOAA <ying.lin@noaa.gov>
2019-09-14 03:13:07 +0000

    update

--- suggested_LSF_settings/pcprtma/run_retro.sh
+++ suggested_LSF_settings/pcprtma/run_retro.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 NDATE=/gpfs/dell1/nco/ops/nwprod/prod_util.v1.1.2/exec/ips/ndate
 RTMAecf=/gpfs/dell2/emc/verification/noscrub/Ying.Lin/pcpanl/rtma.v2.8.0/suggested_LSF_settings/pcprtma
-date1=2019073113
-date2=2019082612
+date1=2019090113
+date2=2019090414
 
-date=$date2
+date=$date1
 
-while [ $date -ge $date1 ]
+while [ $date -le $date2 ]
 do
   $RTMAecf/PCPRTMA.tmp $date
-  date=`$NDATE -1 $date`
+  date=`$NDATE +1 $date`
 done
 
 exit
